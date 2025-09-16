9eea481ab62a7078ace023e33a84bd288a39b43e
CristobalGL <gp1782@gmail.com>
2025-09-16 11:15:32 -0600

    Se agrega el proyecto

---
+++ src/promedioAlumno.java
@@ -0,0 +1,46 @@
+public class promedioAlumno {
+    private String nombre;
+    private double[] calificaciones = new double[5];
+
+    public promedioAlumno(String nombre, double[] calificaciones) {
+        this.nombre = nombre;
+        this.calificaciones = calificaciones;
+    }
+
+    public double calcularPromedio() {
+        double suma = 0;
+        for (double cal : calificaciones) {
+            suma += cal;
+        }
+        return suma / calificaciones.length;
+    }
+
+    public char obtenerCalificacion(double promedio) {
+        if (promedio <= 50) return 'F';
+        else if (promedio <= 60) return 'E';
+        else if (promedio <= 70) return 'D';
+        else if (promedio <= 80) return 'C';
+        else if (promedio <= 90) return 'B';
+        else if (promedio <= 100) return 'A';
+        else return 'A';
+    }
+
+    public void imprimirResultados() {
+        double promedio = calcularPromedio();
+        char calificacionFinal = obtenerCalificacion(promedio);
+
+        System.out.println("Nombre del estudiante: " + nombre);
+        for (int i = 0; i < calificaciones.length; i++) {
+            System.out.println("Calificación " + (i + 1) + ": " + calificaciones[i]);
+        }
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Calificación: " + calificacionFinal);
+    }
+
+    public static void main(String[] args) {
+        double[] califs = {85, 90, 78, 88, 92};
+        promedioAlumno alumno1 = new promedioAlumno("Cristobal Gonzalez", califs);
+
+        alumno1.imprimirResultados();
+    }
+}
